859b6a8b15f9607ecc868fa2d2c3777080d6a02b
mishka49 <61920146+mishka49@users.noreply.github.com>
2022-06-14 17:28:20 +0300

    fixed

--- Sort/main.py
+++ Sort/main.py
@@ -1,10 +1,10 @@
 import timeit
-from List import List
+from mylist_sort import List
 
 length = input("Enter length of list: ")
-list = List.generate_list(length)
+list_int = List.generate_list(length)
 
-print("Befor sort: " + str(list))
-time_exec = timeit.timeit("List.sort(list)", globals=globals(), number=1)
-print("After sort: " + str(list))
+print("Befor sort: " + str(list_int))
+time_exec = timeit.timeit("List.sort(list_int)", globals=globals(), number=1)
+print("After sort: " + str(list_int))
 print("\nTime: "+str(time_exec))
